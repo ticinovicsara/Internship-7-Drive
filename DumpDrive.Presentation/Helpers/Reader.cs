959e431bb03a91f681514e676dfcf33b896740a8
ticinovicsara <ticinovicsara0@gmail.com>
2024-12-22 23:17:20 +0100

    added email validation

--- DumpDrive.Presentation/Helpers/Reader.cs
+++ DumpDrive.Presentation/Helpers/Reader.cs
@@ -1,4 +1,6 @@
-﻿using DumpDrive.Data.Enums;
+﻿using System.Text;
+using System.Xml;
+using DumpDrive.Data.Enums;
 
 namespace DumpDrive.Presentation.Helpers
 {
@@ -21,36 +23,22 @@ namespace DumpDrive.Presentation.Helpers
             return TryReadNumber(out number);
         }
 
-        //upitno za ovo ad days
-        public static bool TryReadDateTime(string message, out DateTime date)
-        {
-            date = DateTime.UtcNow;
+        //?????
+        //public static bool TryReadStatus(string message, out Status status)
+        //{
+        //    status = Status.Todo;
 
-            Console.WriteLine(message);
-            var input = Console.ReadLine();
+        //    Console.WriteLine(message);
+        //    var input = Console.ReadLine();
+        //    var isStatus = Enum.TryParse(typeof(Status), input, out var inputStatus);
 
-            var isValidInput = int.TryParse(input, out var number);
-            if (isValidInput)
-                date = date.AddDays(number);
+        //    if (isStatus && inputStatus is not null)
+        //    {
+        //        status = (Status)inputStatus;
+        //    }
 
-            return isValidInput;
-        }
-
-        public static bool TryReadStatus(string message, out Status status)
-        {
-            status = Status.Todo;
-
-            Console.WriteLine(message);
-            var input = Console.ReadLine();
-            var isStatus = Enum.TryParse(typeof(Status), input, out var inputStatus);
-
-            if (isStatus && inputStatus is not null)
-            {
-                status = (Status)inputStatus;
-            }
-
-            return isStatus;
-        }
+        //    return isStatus;
+        //}
 
         public static bool TryReadLine(string message, out string line)
         {
@@ -60,7 +48,7 @@ namespace DumpDrive.Presentation.Helpers
             var input = Console.ReadLine();
             var isEmpty = string.IsNullOrWhiteSpace(input);
 
-            if (!isEmpty && input is not null)
+            if (!isEmpty)
                 line = input;
 
             return !isEmpty;
@@ -71,5 +59,54 @@ namespace DumpDrive.Presentation.Helpers
             Console.WriteLine(message);
             input = Console.ReadLine() ?? string.Empty;
         }
-    }
+
+        public static bool TryReadEmail(string message, out string email)
+        {
+            email = string.Empty;
+
+            Console.WriteLine(message);
+            var input = Console.ReadLine();
+            var isEmpty = string.IsNullOrWhiteSpace(input);
+            var isValid = !string.IsNullOrWhiteSpace(input) &&
+                input.Contains("@") &&
+                input.Contains(".") &&
+                input.Split("@").Length == 2 &&
+                input.Split(".").Last().Length >= 2;
+
+            if (isValid)
+                email = input;
+
+            return isValid;
+        }
+
+        public static bool TryReadPassword(string massage, out string password)
+        {
+            password = string.Empty;
+            Console.WriteLine(massage);
+
+            ConsoleKey key;
+            var passBuilder = StringBuilder();
+
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && passBuilder.Length > 0)
+                {
+                    passBuilder.Remove(passBuilder.Length - 1, 1);
+                    Console.Write("\b \b");
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    passBuilder.Append(keyInfo.KeyChar);
+                    Console.Write("*");
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            password = passBuilder.ToString();
+
+            return !string.IsNullOrWhiteSpace(password);
+        }
 }
